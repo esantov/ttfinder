3cb3d057be573972267dfc83db5502a0a12a673a
esantov <138806929+esantov@users.noreply.github.com>
2025-05-13 17:42:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -92,8 +92,8 @@ if not data.empty and len(data.columns) > 1:
     time_vals = data.iloc[:, 0].dropna().values
 
     for col in data.columns[1:]:
-    y_vals = data[col].dropna().values
-    x_vals = time_vals[:len(y_vals)]
+        y_vals = data[col].dropna().values
+        x_vals = time_vals[:len(y_vals)]
 
     with st.expander(f"{col}"):
         model_choice = st.selectbox("Choose model", ["5PL", "4PL", "Sigmoid", "Linear"], key=f"model_{col}")
