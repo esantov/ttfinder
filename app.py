5dc2a484685b52f313f299b1db5f508a0984cbf0
esantov <138806929+esantov@users.noreply.github.com>
2025-05-15 10:11:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -128,8 +128,8 @@ for row in summary_rows:
 
     pd.DataFrame(param_rows).to_excel(writer, sheet_name="Fit Parameters", index=False)
 
-        for sample, df in fit_results.items():
-            df.to_excel(writer, sheet_name=sample[:31], index=False)
+    for sample, df in fit_results.items():
+        df.to_excel(writer, sheet_name=sample[:31], index=False)
     output.seek(0)
     return output
 
